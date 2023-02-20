85fcddca278546b48fd4954e7670a180b0978187
vvsomov <vvsomov@sbercloud.ru>
2023-02-20 11:28:58 +0400

    add demo template

--- src/main/kotlin/ru/vassuv/plugin/createfromtemplate/model/Const.kt
+++ src/main/kotlin/ru/vassuv/plugin/createfromtemplate/model/Const.kt
@@ -1,6 +1,7 @@
 package ru.vassuv.plugin.createfromtemplate.model
 
 object Const {
+
     const val JSON_NAME_FILE = "ModuleGenerator"
 
     const val CREATE_FROM_TEMPLATES = "Create from templates"
@@ -11,4 +12,53 @@ object Const {
     const val VALUE_COLUMN_CAN_BE_EDITED = "* Value column can be edited"
     const val PROPERTY = "Property"
     const val VALUE = "Value"
+
+    object Demo {
+        val JSON_FILE: String get() = """
+            {
+               "module_name": "Sample",
+               "app_package": "com.example.app",
+               "module_package": "{{.app_package}}.{{.module_name.lowercase()}}",
+               "module_path": "{{.module_package.replace('.', '/')}}"
+            }
+        """.trimIndent()
+        val GRADLE_FILE: String get() = """
+            apply plugin: 'com.android.library'
+            apply plugin: 'kotlin-android'
+            apply plugin: 'kotlin-kapt'
+            
+            android {
+              namespace = "{{.module_package}}"
+            }
+            
+            dependencies {
+            }
+        """.trimIndent()
+        val GIT_IGNORE_FILE: String get() = """
+            /build
+        """.trimIndent()
+        val README_FILE: String get() = """
+            There will be information about the module "{{.module.name}}"
+        """.trimIndent()
+        val ANDROID_MANIFEST_FILE: String get() = """
+            <manifest />
+        """.trimIndent()
+        val REPOSITORY_FILE: String get() = """
+            package {{.module_package}}
+    
+            import {{.module_package}}.entity.{{.module_name}}
+    
+            interface {{.module_name}}Repository {
+              fun get{{.module_name}}(): {{.module_name}} 
+            }
+
+        """.trimIndent()
+        val REPOSITORY_ENTITY_FILE: String get() = """
+            package {{.module_package}}.entity
+            
+            class {{.module_name}} {
+               val id: String
+            }
+        """.trimIndent()
+    }
 }
--- src/main/kotlin/ru/vassuv/plugin/createfromtemplate/model/TemplatesModel.kt
+++ src/main/kotlin/ru/vassuv/plugin/createfromtemplate/model/TemplatesModel.kt
@@ -4,6 +4,7 @@ import ru.vassuv.plugin.createfromtemplate.model.entity.Template
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import ru.vassuv.plugin.createfromtemplate.model.Const.JSON_NAME_FILE
+import java.io.File
 
 class TemplatesModel(
     private val project: Project,
@@ -16,9 +17,65 @@ class TemplatesModel(
     private val templates: List<TemplateTreeNode>
 
     init {
-        val rootFolder = project.workspaceFile!!.parent.parent
-        val templatesFolder = rootFolder.children.firstOrNull { it.isDirectory && it.name == ".templates"  }
-        templates = (templatesFolder?.children?.mapToTreeNode(0) ?: arrayListOf()).optimize()
+        this.templates = loadTemplateTreeNodes()
+
+    }
+
+    private fun loadTemplateTreeNodes(): List<TemplateTreeNode> {
+        val rootFolder = project.workspaceFile?.parent?.parent ?: return listOf()
+        val templatesFolder = rootFolder.children.firstOrNull { it.isDirectory && it.name == ".templates" } ?: run {
+            addDemo(rootFolder.path + "/.templates")
+            rootFolder.refresh(false, true)
+            rootFolder.children.firstOrNull { it.isDirectory && it.name == ".templates" }
+        } ?: return emptyList()
+        var templates = templatesFolder.children.mapToTreeNode(0).optimize()
+        if (templates.isEmpty()) {
+            addDemo(templatesFolder.path)
+            templatesFolder.refresh(false, true)
+            templates = templatesFolder.children.mapToTreeNode(0).optimize()
+        }
+        return templates
+    }
+
+    private fun addDemo(path: String) {
+        val demoPath = "$path/demo_module"
+        File(demoPath).mkdirs()
+
+        val readme = File("$demoPath/{{.module_name}}.md")
+        readme.createNewFile()
+        readme.writeText(Const.Demo.README_FILE)
+
+        val jsonFile = File("$demoPath/ModuleGenerator.json")
+        jsonFile.createNewFile()
+        jsonFile.writeText(Const.Demo.JSON_FILE)
+
+        val modulePath = "$demoPath/{{.module_name.lowercase()}}"
+        File(modulePath).mkdir()
+
+        val gradleFile = File("$modulePath/build.gradle")
+        gradleFile.createNewFile()
+        gradleFile.writeText(Const.Demo.GRADLE_FILE)
+
+        val gitIgnore = File("$modulePath/.gitignore")
+        gitIgnore.createNewFile()
+        gitIgnore.writeText(Const.Demo.GIT_IGNORE_FILE)
+        val manifestFolderPath = "$modulePath/src/main"
+        File(manifestFolderPath).mkdirs()
+        val androidManifest = File("$manifestFolderPath/AndroidManifest.xml")
+        androidManifest.writeText(Const.Demo.ANDROID_MANIFEST_FILE)
+
+        val sourceFolderPath = "$manifestFolderPath/kotlin/{{.module_path}}"
+        File(sourceFolderPath).mkdirs()
+        val repository = File("$sourceFolderPath/{{.module_name}}Repository.kt")
+        repository.writeText(Const.Demo.REPOSITORY_FILE)
+
+        val entityFolderPath = "$sourceFolderPath/.entity"
+        File(entityFolderPath).mkdir()
+        val entity = File("$entityFolderPath/{{.module_name}}.kt")
+        entity.writeText(Const.Demo.REPOSITORY_ENTITY_FILE)
+
+        val resFolderPath = "$manifestFolderPath/res"
+        File(resFolderPath).mkdir()
     }
 
     fun getTemplateTree(): List<Template> {
