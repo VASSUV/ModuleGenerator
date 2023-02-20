4e2ef36d099a1facf4cd1886a62d0a10cace0e32
vvsomov <vvsomov@sbercloud.ru>
2023-02-20 14:22:14 +0400

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.vassuv.plugin.createfromtemplate"
-version = "0.1-alpha"
+version = "0.1.3"
 
 repositories {
     mavenCentral()
@@ -36,7 +36,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("212")
-//        untilBuild.set("222.*")
+        untilBuild.set("223.*")
     }
 
     signPlugin {
@@ -47,6 +47,5 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
-        channels.set(listOf("beta"))
     }
 }
